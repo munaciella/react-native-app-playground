f1c1f54810510293b07aea70b20068196dbe3803
munaciella <franes88@gmail.com>
2024-01-30 15:04:41 +0000

    add code sign in & sign up comp

--- Screens/SignUp.tsx
+++ Screens/SignUp.tsx
@@ -1,28 +1,122 @@
 import React from "react";
-import {Button, StyleSheet, Text, View} from "react-native"
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+} from 'react-native';
 
 const SignUp = () => {
     return (
-        <View style={styles.mainView}>
-        <Text> Sign Up </Text>
-        <Button
-            title="Sign Up"
-            />
+      <View style={styles.mainView}>
+      <View style={styles.TopView}>
+        <Image
+          style={styles.ImageStyle}
+          source={require('./../assets/icon.png')}
+        />
+      </View>
+      <View style={styles.BottomView}>
+        <Text style={styles.Heading}>Welcome{'\n'} Back </Text>
+        <View style={styles.FormView}>
+          <TextInput
+            placeholder={'Email'}
+            placeholderTextColor={'white'}
+            style={styles.TextInput}
+          />
+          <TextInput
+            placeholder={'Password'}
+            secureTextEntry={true}
+            placeholderTextColor={'white'}
+            style={styles.TextInput}
+          />
+          <TouchableOpacity style={styles.Button}>
+            <Text style={styles.ButtonText}>Sign In</Text>
+          </TouchableOpacity>
         </View>
+      </View>
+    </View>
     )
 }
 
 const styles = StyleSheet.create({
-      mainView: {
-        marginTop: 40,
-        flex:1,
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center"
-      },
-      textStyle: {
-        color: "blue"
-      }
-    })
+  mainView: {
+    marginTop: 40,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  TopView: {
+    width: '100%',
+    height: '15%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  BottomView: {
+    width: '100%',
+    height: '85%',
+    backgroundColor: 'black',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: 20
+  },
+  ImageStyle: {
+    width: 150,
+    height: 150,
+    marginBottom: 15,
+  },
+  Heading: {
+    color: '#fff',
+    fontSize: 42,
+    fontWeight: 'bold',
+    marginLeft: 40,
+    marginTop: 60,
+  },
+  TextInput: {
+    width: '90%',
+    borderWidth: 1,
+    borderColor: 'white',
+    height: 50,
+    borderRadius: 10,
+    paddingLeft: 10,
+    marginTop: 10,
+    color: 'white',
+  },
+  FormView: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  Button: {
+    width: '90%',
+    color: '#000',
+    height: 50,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    marginTop: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  SignUpText: {
+    color: 'gray',
+  },
+  TextButton: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+});
 
 export default SignUp
--- Screens/Signin.tsx
+++ Screens/Signin.tsx
@@ -39,6 +39,9 @@ const SignIn = ({navigation}) => {
             <Text style={styles.ButtonText}>Sign In</Text>
           </TouchableOpacity>
         </View>
+        <TouchableOpacity style={styles.TextButton} onPress={navigate}>
+        <Text style={styles.SignUpText}>Sign up</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -110,6 +113,15 @@ const styles = StyleSheet.create({
   ButtonText: {
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  SignUpText: {
+    color: 'gray',
+  },
+  TextButton: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: 20,
   },
 });
 
